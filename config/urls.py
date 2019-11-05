8a17b5045f5c35dc1d12c44a2952e49d134be699
Bruno Cochard <bruno.cochard@gmail.com>
2019-11-05 14:31:00 +0100

    error on url with media

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,10 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
-    path(r'^$' , TemplateView.as_view(template_name='index.html')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG is True:
+    urlpatterns += path(r'^$' , TemplateView.as_view(template_name='index.html')),
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
